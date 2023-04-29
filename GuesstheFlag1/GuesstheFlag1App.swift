4c364344ecc9b20ae01a7b255e6c266cf88b2759
jirimatousek <jiri.matousek1999@gmail.com>
2023-04-29 19:41:20 +0200

    Initial Commit

---
+++ GuesstheFlag1/GuesstheFlag1App.swift
@@ -0,0 +1,17 @@
+//
+//  GuesstheFlag1App.swift
+//  GuesstheFlag1
+//
+//  Created by Jiri Matousek on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuesstheFlag1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
